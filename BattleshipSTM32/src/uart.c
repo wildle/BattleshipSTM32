570d2d729dae4a9ad268ab9fb99f11025f3b2c25
Lenard <le.wild@mci4me.at>
2024-06-09 12:41:08 +0200

    chore: Update UART initialization to enable receive interrupt in USART2

--- BattleshipSTM32/src/uart.c
+++ BattleshipSTM32/src/uart.c
@@ -15,7 +15,7 @@ void UART_Init(void) {
     // - Baud rate: 9600
     // - 8 data bits, 1 stop bit, no parity, no flow control
     USART2->BRR = (SystemCoreClock / 9600);   // Assuming 48MHz PCLK
-    USART2->CR1 = USART_CR1_TE | USART_CR1_RE | USART_CR1_UE;
+    USART2->CR1 = USART_CR1_TE | USART_CR1_RE | USART_CR1_UE | USART_CR1_RXNEIE;
 
     // Enable USART2 interrupt
     NVIC_SetPriority(USART2_IRQn, 1);
@@ -43,5 +43,8 @@ void USART2_IRQHandler(void) {
     if (USART2->ISR & USART_ISR_RXNE) {
         char c = (char)(USART2->RDR); // Read received data
         UART_SendChar(c); // Echo received character
+
+        // Debug: Toggle an LED or send a debug message
+        // GPIOA->ODR ^= GPIO_ODR_5; // Toggle PA5 (assuming an LED is connected)
     }
 }
